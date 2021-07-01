4434fcca7778d8b0c8db4d49e6432800db60578a
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-01 13:40:05 +0700

    GII crud

---
+++ models/Article.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "articles".
+ *
+ * @property int $id
+ * @property int|null $uid
+ * @property string|null $title
+ * @property string|null $content
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'articles';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'required'],
+            [['id', 'uid'], 'default', 'value' => null],
+            [['id', 'uid'], 'integer'],
+            [['title', 'content'], 'string'],
+            [['id'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'uid' => 'Uid',
+            'title' => 'Title',
+            'content' => 'Content',
+        ];
+    }
+}
---
+++ views/article/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Article */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="article-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id')->textInput() ?>
+
+    <?= $form->field($model, 'uid')->textInput() ?>
+
+    <?= $form->field($model, 'title')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
