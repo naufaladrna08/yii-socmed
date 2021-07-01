9636c5979327aa9226e7b54c9d4694f7218797df
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-01 09:54:16 +0700

    nyoba bikin form

---
+++ models/EntryForm.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\Models;
+
+use Yii;
+use yii\base\Model;
+
+class EntryForm extends Model {
+  public $name;
+  public $email;
+
+  public function rules() {
+    return [
+      [['name', 'email'], 'required'],
+      ['email', 'email']
+    ];
+  }
+}
