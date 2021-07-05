c2cdfbc520b0b1f75cd4edd832f2957e93ae8f48
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-05 15:55:44 +0700

    redesign

--- views/site/login.php
+++ views/site/login.php
@@ -10,26 +10,67 @@ use yii\bootstrap4\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-  <h1><?= Html::encode($this->title) ?></h1>
 
-  <p>Please fill out the following fields to login:</p>
+<style>
+  html,
+  body {
+    height: 100%;
+    background-color: #333;
+  }
 
-  <?php $form = ActiveForm::begin([
-    'id' => 'login-form',
-    'layout' => 'horizontal'
-  ]); ?>
+  body {
+    display: -ms-flexbox;
+    display: flex;
+    color: #fff;
+    text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
+    box-shadow: inset 0 0 5rem rgba(0, 0, 0, .5);
+  }
 
-    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-    <?= $form->field($model, 'password')->passwordInput() ?>
-    <?= $form->field($model, 'rememberMe')->checkbox() ?>
+  .cover-container {
+    max-width: 42em;
+  }
+</style>
 
-    <div class="form-group">
-      <div class="col-lg-offset-1 col-lg-12 px-0">
-        <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-        <?= Html::a('Register', ['/site/register'], ['class'=>'btn btn-success']) ?>
-      </div>   
-    </div>
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+      <div class="inner">
+        <h3 class="masthead-brand">Cover</h3>
+        <nav class="nav nav-masthead justify-content-center">
+          <a class="nav-link active" href="#">Home</a>
+          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Contact</a>
+        </nav>
+      </div>
+    </header>
 
-  <?php ActiveForm::end(); ?>
-</div>
+    <main role="main" class="inner cover text-center" style="width: 80%;">
+      <div class="title my-4">
+        <h1><?= Html::encode($this->title) ?></h1>
+        <p class="lead"> Please fill this form to Login! </p>
+      </div>
+
+      <?php $form = ActiveForm::begin([
+        'id' => 'login-form',
+        'class' => 'text-center'
+      ]); ?>
+      
+        <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label(false) ?>
+        <?= $form->field($model, 'password')->passwordInput()->label(false) ?>
+        <?= $form->field($model, 'rememberMe')->checkbox() ?>
+
+        <div class="form-group">
+          <div class="col-lg-offset-1 col-lg-12 px-0">
+            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            <?= Html::a('Register', ['/site/register'], ['class'=>'btn btn-success']) ?>
+          </div>   
+        </div>
+
+      <?php ActiveForm::end(); ?>
+    </main>
+
+    <footer class="mastfoot mt-auto">
+      <div class="inner">
+        <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+      </div>
+    </footer>
+  </div>
