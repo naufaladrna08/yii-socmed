a8ae859bbdf72d25afc9f0c04645418524e4cf18
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-06 12:35:44 +0700

    create article moved to site/create-article

--- views/site/index.php
+++ views/site/index.php
@@ -2,7 +2,6 @@
 
 /* @var $this yii\web\View */
 
-use Yii;
 use yii\helpers\Html;
 use yii\helpers\textarea;
 use yii\bootstrap4\ActiveForm;
@@ -23,38 +22,21 @@ $this->params['breadcrumbs'][] = "";
     </div>
 
     <div class="col-md-8">
-    <a class="btn btn-primary mb-4" data-toggle="collapse" href="#create-article-form" role="button" aria-expanded="false" aria-controls="collapseExample">
-      Create a new Article
-    </a>
+      <?= Html::a('Create an Article', ['site/create-article'], ['class' => 'btn btn-primary mb-4']) ?>
 
-    <div class="collapse my-2" id="create-article-form">
-      <?php $form = ActiveForm::begin(['id' => 'create-article-form']) ?>
-      
-      <?= $form->field($new, 'title')->textInput() ?>
-      <?= $form->field($new, 'content')->textarea() ?>
-
-      <div class="form-group">
-        <div class="col-lg-offset-1 col-lg-12 px-0">
-          <?= Html::submitButton('Post', ['class' => 'btn btn-primary', 'name' => 'create-button']) ?>
-        </div>   
-      </div>
-
-      <?php ActiveForm::end()  ?>
-    </div>
-
-    <?php foreach ($articles as $a): ?>
-      <div class="media border rounded p-2">
+      <?php foreach ($model as $article): ?>
+      <div class="media border rounded p-2 my-2">
         <img class="mr-3 img-thumbnail" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" width="64" alt="Generic placeholder image">
         <div class="media-body">
-          <h5 class="mt-0"> <?= $a['title'] ?> </h5>
-          <?= $a['content'] ?>
+          <h5 class="mt-0"> <?= $article['title'] ?> </h5>
+          <?= Html::encode(strip_tags($article['content'])) ?>
 
           <div class="media-footer mt-2">
-            ~ <?= Html::a($a['user']['username'], ['user/', 'username' => $a['user']['username']]) ?> <br>
+            ~ <?= Html::a($article['user']['username'], ['user/', 'username' => $article['user']['username']]) ?> <br>
           </div>
         </div>
       </div>
-    <?php endforeach; ?>
+      <?php endforeach; ?>
     </div>
     
     <div class="col-md-2">
