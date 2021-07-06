43b40fdf16c7354cd6e17149ec777c0b41f8ce75
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-06 14:15:10 +0700

    pictures model

---
+++ models/Picture.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "pictures".
+ *
+ * @property int $id
+ * @property int|null $author
+ * @property string|null $link
+ */
+class Picture extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'pictures';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['author'], 'default', 'value' => null],
+            [['author'], 'integer'],
+            [['link'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'author' => 'Author',
+            'link' => 'Link',
+        ];
+    }
+}
