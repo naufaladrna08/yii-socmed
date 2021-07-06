51300be7510169a6540d3e40c3da8a69f58b265d
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-06 13:51:46 +0700

    add like or dislike button

--- views/site/index.php
+++ views/site/index.php
@@ -25,6 +25,11 @@ $this->params['breadcrumbs'][] = "";
       <?= Html::a('Create an Article', ['site/create-article'], ['class' => 'btn btn-primary mb-4']) ?>
 
       <?php foreach ($model as $article): ?>
+      <?php 
+        $thumb = $like->find()->where(['uid' => Yii::$app->user->identity->id, 'aid' => $article['id']])->count() == 1 ? '<i class="fa fa-thumbs-up"> </i> ' : '<i class="fa fa-thumbs-down"> </i> ';
+        $likes = $thumb . $like->find()->where(['aid' => $article['id']])->count();   
+      ?>
+      
       <div class="media border rounded p-2 my-2">
         <img class="mr-3 img-thumbnail" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" width="64" alt="Generic placeholder image">
         <div class="media-body">
@@ -33,7 +38,9 @@ $this->params['breadcrumbs'][] = "";
 
           <div class="media-footer mt-2">
             ~ <?= Html::a($article['user']['username'], ['user/', 'username' => $article['user']['username']]) ?> <br>
+            <?= Html::a($likes, 'article/like') ?>
           </div>
+          <br>
         </div>
       </div>
       <?php endforeach; ?>
