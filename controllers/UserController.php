5199c9127f4b7c1c9856f5294886c2ee2e80d4c1
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-06 16:25:40 +0700

    change photo

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -4,6 +4,8 @@ namespace app\controllers;
 
 use Yii;
 use \app\models\AppUser;
+use \app\models\Picture;
+use yii\web\UploadedFile;
 
 class UserController extends \yii\web\Controller {
   public function actionIndex($username = "") {
@@ -16,5 +18,29 @@ class UserController extends \yii\web\Controller {
     }
 
     return $this->render('index', ['data' => $user, 'username' => $username]);
+  }
+
+  public function actionChangePhoto() {
+    $model = new Picture();
+    if (Yii::$app->request->isPost) {
+      /* Save photo */
+      $model->file = UploadedFile::getInstance($model, 'file');
+      $fileName = 'uploads/' .  time() . '.' . $model->file->extension;
+      $model->file->saveAs($fileName);
+      $model->author = Yii::$app->user->identity->id;
+      $model->link   = $fileName;
+      $model->save();
+
+      unset($model);
+      /* Update userdata */
+      $model = Picture::find()->where(['link' => $fileName])->one();
+      $user = AppUser::findOne(Yii::$app->user->identity->id);
+      $user->photo = $model->id;
+      $user->save();
+
+      return $this->redirect(['user/' . Yii::$app->user->identity->username]);
+    }
+
+    return $this->render('change-photo', ['model' => $model]);
   }
 }
