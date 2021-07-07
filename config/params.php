f43c100709e9d0764eb29a2bcd2b1ceb85e4eb4f
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-07 16:37:54 +0700

    tinify token

--- config/params.php
+++ config/params.php
@@ -4,4 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'tinify-api-token' => 'mPkn4VhQkcQtXjGsPTY9ZS19gpt5srjF'
 ];
